d60ef21ed3f863a44a0cf2ca4e41ad3829eb0eef
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-13 14:31:06 +0100

    create new sandbox account with aft

---
+++ terraform/account-request-sandbox.tf
@@ -0,0 +1,27 @@
+module "sandbox_account_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sandbox-account-01@gmail.com"
+    AccountName               = "sandbox-account-01"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "sandbox-account-01@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Sandbox_account_01" = "sandbox-account-01@gmail.com"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "rihem"
+    change_reason       = "provisioning a new sandbox account"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Sandbox"
+}
