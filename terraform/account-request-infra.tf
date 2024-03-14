527bb9ee86b8c3011cbaf16cd92509d655ee850f
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-14 08:58:36 +0100

    create new infra account with aft inside OU

--- terraform/account-request-infra.tf
+++ terraform/account-request-infra.tf
@@ -1,17 +1,17 @@
-module "infra_account_prod_01" {
+module "infra_account_prod_02" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "infra-account-prod-01@gmail.com"
-    AccountName               = "infra-account-prod-01"
+    AccountEmail              = "infra-account-prod-02@gmail.com"
+    AccountName               = "infra-account-prod-02"
     ManagedOrganizationalUnit = "ou-6vtm-2mjw9h87"
-    SSOUserEmail              = "infra-account-prod-01@gmail.com"
-    SSOUserFirstName          = "Infra"
-    SSOUserLastName           = "PROD"
+    SSOUserEmail              = "infra-account-prod-02@gmail.com"
+    SSOUserFirstName          = "InfraProd2"
+    SSOUserLastName           = "PROD2"
   }
 
   account_tags = {
-    "infra-account-prod-01" = "infra-account-prod-01@gmail.com"
+    "infra-account-prod-01" = "infra-account-prod-02@gmail.com"
   }
 
   change_management_parameters = {
