13db4856274fe081870ab3e969ec651f9ccf8924
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-14 14:55:36 +0100

    create new infra test account with aft inside OU

--- terraform/account-request-infra.tf
+++ terraform/account-request-infra.tf
@@ -4,7 +4,7 @@ module "infra_account_test_01" {
   control_tower_parameters = {
     AccountEmail              = "infra-account-test-01@gmail.com"
     AccountName               = "infra-account-test-01"
-    ManagedOrganizationalUnit = "Test" 
+    ManagedOrganizationalUnit = "Infrastructure" 
     SSOUserEmail              = "infra-account-test-01@gmail.com"
     SSOUserFirstName          = "Infra"
     SSOUserLastName           = "AFT"
@@ -26,31 +26,4 @@ module "infra_account_test_01" {
   account_customizations_name = "Infra"
 }
 
-module "infra_account_prod_01" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "infra-account-prod-01@gmail.com"
-    AccountName               = "infra-account-prod-01"
-    ManagedOrganizationalUnit = "ou-6vtm-2mjw9h87"
-    SSOUserEmail              = "infra-account-prod-01@gmail.com"
-    SSOUserFirstName          = "Infra"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "infra-account-prod-01" = "infra-account-prod-01@gmail.com"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "rihem"
-    change_reason       = "provisioning new infra prod account"
-  }
-
-  custom_fields = {
-    group = "prod"
-  }
-
-  account_customizations_name = "Infra"
-}
 
