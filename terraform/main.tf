067b0c3a57f9f9f808f4d7b7a7657238768a97ae
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-14 13:36:14 +0100

    create new workload test account with aft inside OU

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+module "workloads" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "workloads-test-account-01@gmail.com"
+    AccountName               = "workloads-test"
+    ManagedOrganizationalUnit = "Workloads"
+    SSOUserEmail              = "workloads-test-account-01@gmail.com"
+    SSOUserFirstName          = "workloads-test"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "workloads-test-account-01@gmail.com" = "workloads-test-account-01@gmail.com"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "rihem"
+    change_reason       = "add workloads test account"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
