07c05175ae8c89cc32656bc19076f652fedbcfe2
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-15 11:28:36 +0100

    create new sandbox account with aft inside OU

--- terraform/account-request-sandbox.tf
+++ terraform/account-request-sandbox.tf
@@ -26,5 +26,33 @@ module "sandbox_account_01" {
   account_customizations_name = "Sandbox"
 }
 
+module "sandbox_account_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sandbox-account-02@gmail.com"
+    AccountName               = "sandbox-account-02"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "sandbox-account-02@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Sandbox_account_02" = "sandbox-account-02@gmail.com"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "rihem"
+    change_reason       = "provisioning a new sandbox account"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Sandbox"
+}
+
 
 
