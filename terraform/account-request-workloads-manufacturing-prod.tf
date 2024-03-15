6f8afb41317dc6fcf4d2b0f9193a989e545c7b43
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-15 14:54:14 +0100

    create new account with aft inside OU

--- terraform/account-request-workloads-manufacturing-prod.tf
+++ terraform/account-request-workloads-manufacturing-prod.tf
@@ -4,7 +4,7 @@ module "workloads_manufacturing_prod" {
   control_tower_parameters = {
     AccountEmail              = "workloads_data_manufacturing_prod-account@gmail.com"
     AccountName               = "workloads_data_manufacturing_prod"
-    ManagedOrganizationalUnit = "ou-6vtm-aj0rx5wt"
+    ManagedOrganizationalUnit = "Prod"
     SSOUserEmail              = "workloads_data_manufacturing_prod@gmail.com"
     SSOUserFirstName          = "workloads_data_manufacturing_prod"
     SSOUserLastName           = "prod"
