6dfc3e8d56a908bdcc89817ca4e465b764c4967d
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-15 14:50:57 +0100

    create new account with aft inside OU

---
+++ terraform/account-request-workloads-manufacturing-prod.tf
@@ -0,0 +1,27 @@
+module "workloads_manufacturing_prod" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "workloads_data_manufacturing_prod-account@gmail.com"
+    AccountName               = "workloads_data_manufacturing_prod"
+    ManagedOrganizationalUnit = "ou-6vtm-aj0rx5wt"
+    SSOUserEmail              = "workloads_data_manufacturing_prod@gmail.com"
+    SSOUserFirstName          = "workloads_data_manufacturing_prod"
+    SSOUserLastName           = "prod"
+  }
+
+  account_tags = {
+    "workloads_data_manufacturing_prod@gmail.com" = "workloads_data_manufacturing_prod@gmail.com"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "rihem"
+    change_reason       = "add workloads prod account"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
