666f7c31a884053fa47b0a6766f40b10289747f4
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-19 09:47:32 +0100

    create new account with aft inside OU

---
+++ terraform/account-request-infra-network-prod.tf
@@ -0,0 +1,28 @@
+module "network-prod-account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "network-prod-account@gmail.com"
+    AccountName               = "network-prod-account"
+    ManagedOrganizationalUnit = "Infrastructure" 
+    SSOUserEmail              = "network-prod-account@gmail.com"
+    SSOUserFirstName          = "network"
+    SSOUserLastName           = "prod"
+  }
+
+  account_tags = {
+    "network-prod" = "network-prod-account@gmail.com"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "rihem"
+    change_reason       = "provisioning new infra prod account"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
