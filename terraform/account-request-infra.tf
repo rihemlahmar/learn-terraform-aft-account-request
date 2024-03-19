6d102b7c415208f11d771c4a77a800955454a66d
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-19 10:56:12 +0100

    create new account with aft inside OU

---
+++ terraform/account-request-infra.tf
@@ -0,0 +1,32 @@
+module "network_test_account_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "network-test-account-01@gmail.com"
+    AccountName               = "network-test-account-01"
+    ManagedOrganizationalUnit = "Test (ou-6vtm-8xv2vqfm)"
+    SSOUserEmail              = "network-test-account-01@gmail.com"
+    SSOUserFirstName          = "network"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "network-test" = "network-test-account-01@gmail.com"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "rihem"
+    change_reason       = "provisioning a new account"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Sandbox"
+}
+
+
+
+
+
