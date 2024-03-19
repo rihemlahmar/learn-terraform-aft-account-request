b8721f217e9af0accc73441b1d6f11da2fdf9735
rihemlahmar <rihemlahmar01@gmail.com>
2024-03-19 14:32:01 +0100

    create new account with aft inside OU

--- terraform/account-request-shared-cicd-prod.tf
+++ terraform/account-request-shared-cicd-prod.tf
@@ -4,7 +4,8 @@ module "shared-cicd-prod" {
   control_tower_parameters = {
     AccountEmail              = "shared-cicd-prod-account@gmail.com"
     AccountName               = "shared-cicd-prod-account"
-    ManagedOrganizationalUnit = "Prod(ou-6vtm-r2gnwgt1)"
+    //ManagedOrganizationalUnit = "Prod(ou-6vtm-r2gnwgt1)"
+    ManagedOrganizationalUnit = "Deployments"
     SSOUserEmail              = "shared-cicd-prod-account@gmail.com"
     SSOUserFirstName          = "infra"
     SSOUserLastName           = "AFT"
